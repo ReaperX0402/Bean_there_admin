66dfeae101e8fd127ba8de168feb60ff3b83428e
Darren <darrenljy04@gmail.com>
2025-11-17 11:54:53 +0800

    Updated the login and supabase_config

--- js/common/supabase_config.js
+++ js/common/supabase_config.js
@@ -1,6 +1,2 @@
-// Update the placeholders below with your Supabase project credentials.
-// This file is loaded before the rest of the app and should define
-// `window.SUPABASE_CONFIG` using the `url` and `anonKey` variables.
-
 window.SUPABASE_CONFIG = { url: 'https://gaotkltiafwezuzegkkz.supabase.co', 
     anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdhb3RrbHRpYWZ3ZXp1emVna2t6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIyMzQ2NDYsImV4cCI6MjA3NzgxMDY0Nn0.BH3tHLwuriDLxuZHRPmkM7kBFBwZRDjJemZ2YXE-kK8'};
